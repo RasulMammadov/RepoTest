0d403c181fc4100702ec8b9f3e54244f7e8e5bf2
Rasul Mammadov <Rasul.Mammadov2@AcceessBank.az>
2025-02-17 17:02:56 +0400

    Commit after merge changes

---
+++ GitRepoTest/Controllers/TestCustom2Controller.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitRepoTest.Controllers
+{
+    [ApiController]
+    [Route("TestCustom2")]
+    public class TestCustom2Controller : ControllerBase
+    {
+        [HttpPost("/TestcustomRequest")]
+        public IActionResult Index(Request request)
+        {
+           var req = HttpContext.Request;
+            return Ok(new { Id = request.id, Name = request.Name, ID2 = 35, Name2 = "DUDD", Course = 3 });
+        }
+        public class Request
+        {
+            public int id { get; set; }
+            public string Name { get; set; }
+        }
+    }
+}
---
+++ GitRepoTest/gRPC/Services/TestGrpcService.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using static GitRepoTest.gRPC.TestgRPC;
+
+namespace GitRepoTest.gRPC.Services
+{
+    public class TestGrpcService : TestgRPCBase
+    {
+        public override Task<TestResponseType> TestBasics(TestRequestType request, ServerCallContext context)
+        {
+            return base.TestBasics(request, context);
+
+        }
+
+        public override Task TestStreams(IAsyncStreamReader<TestRequestType> requestStream, IServerStreamWriter<TestResponseType> responseStream, ServerCallContext context)
+        {
+            return Task.FromResult(23);
+        }
+        
+    }
+}
