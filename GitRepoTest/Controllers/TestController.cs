3a59da0b2f3cc268fc37ead511ea5ce4e0a9ca2d
Rasul Mammadov <Rasul.Mammadov2@AcceessBank.az>
2025-02-18 11:58:29 +0400

    Add untracked changes for static files and add them into remote

---
+++ GitRepoTest/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GitRepoTest.Controllers
+{
+    [ApiController]
+    [Route("[Controller]")]
+    public class TestController : ControllerBase
+    {
+        [HttpGet]
+        [Route("/GetTest")]
+        public ActionResult GetTest()
+        {
+            var request = HttpContext.Request;
+            return Ok(new { Message = "this is get test method", Sender = "You do not need to know" });
+        }
+
+        [HttpPost]
+        [Route("/PostTest")]
+        public ActionResult PostTest()
+        {
+
+            var request = HttpContext.Request;
+            return Ok(new { Message = "this is post test method", Sender = "You do not need to know" });
+        }
+    }
+}
---
+++ GitRepoTest/gRPC/Services/TestGrpcService.cs
@@ -0,0 +1,13 @@
+﻿using Grpc.Core;
+using static GitRepoTest.gRPC.TestService;
+
+namespace GitRepoTest.gRPC.Services
+{
+    public class TestGrpcService : TestServiceBase
+    {
+        public override Task<TestResponse> TestMethod1(Testrequest request, ServerCallContext context)
+        {
+            return base.TestMethod1(request, context);
+        }
+    }
+}
